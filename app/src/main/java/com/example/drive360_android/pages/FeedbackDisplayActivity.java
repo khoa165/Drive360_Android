a8799e8f1eae72afd4163bb64004044fe7fdca6e
monica22schmidt <monica22schmidt@gmail.com>
2020-05-01 21:44:05 -0500

    feedbackDisplayActivity

---
+++ app/src/main/java/com/example/drive360_android/pages/FeedbackDisplayActivity.java
@@ -0,0 +1,13 @@
+package com.example.drive360_android.pages;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class FeedbackDisplayActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_feedback_display);
+    }
+}
